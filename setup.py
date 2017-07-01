dd44c515ef744f681bb9737df5b89f946c98c6e3
Éric Massé <Ericmas001@gmail.com>
2017-07-01 18:44:00 -0400

    still trying with setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import os
 from setuptools import setup, find_packages
 
 # Get __version__ which is stored in python/version.py
-ver_file = os.path.join('python', 'version.py')
+ver_file = os.path.join('python', 'hvac_ircontrol', 'version.py')
 exec(open(ver_file).read())
 
 setup(
     name='hvac_ircontrol',
+    package_dir = {'': 'python'},
     version=__version__,
-    packages=find_packages('./python'),
+    packages=find_packages(srcfile('python')),
     namespace_packages=['hvac_ircontrol'],
     url='',
     license='',
