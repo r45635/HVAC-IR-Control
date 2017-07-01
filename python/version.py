81fd8f3e7860264539b55fd61c0b73ec9ebea76d
Éric Massé <Ericmas001@gmail.com>
2017-07-01 18:17:00 -0400

    Adding setup.py

---
+++ python/version.py
@@ -0,0 +1,17 @@
+# Version information.  
+_version_major = 1
+_version_minor = 0
+_version_micro = 0
+
+
+# An empty _version_extra corresponds to a full release.
+# '.dev' as a _version_extra string means this is a development version
+_version_extra = ''
+# _version_extra = 'dev'
+
+
+# Format expected by setup.py: string of form "X.Y.Z"
+__version__ = "%s.%s.%s%s" % (_version_major,
+                              _version_minor,
+                              _version_micro,
+                              _version_extra)
---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+# Get __version__ which is stored in src/version.py
+ver_file = os.path.join('python', 'version.py')
+exec(open(ver_file).read())
+
+setup(
+    name='hvac_ircontrol',
+    version=__version__,
+    packages=find_packages(),
+    namespace_packages=['hvac_ircontrol'],
+    url='',
+    license='',
+    author='ericmas001',
+    author_email='ericmas001@gmail.com',
+    description=''
+)
