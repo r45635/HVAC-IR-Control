63dbf971514c4ae69723724df2be51098e0eb0f3
Éric Massé <Ericmas001@gmail.com>
2017-07-01 22:56:43 -0400

    Supporting everything for MSZ-FE12NA

--- python/hvac_ircontrol/mitsubishi.py
+++ python/hvac_ircontrol/mitsubishi.py
@@ -7,46 +7,106 @@
 
 import ir_sender
 import pigpio
+from datetime import datetime
 
 class PowerMode:
     """
     PowerMode
     """
-    PowerOff = 0b00000000   # 0x00      0000 0000        0
-    PowerOn = 0b00100000    # 0x20      0010 0000       32
+    PowerOff = 0b00000000       # 0x00      0000 0000        0
+    PowerOn = 0b00100000        # 0x20      0010 0000       32
 
 class ClimateMode:
     """
     ClimateMode
     """
-    Hot = 0b00001000        # 0x08      0000 1000        8
-    Cold = 0b00011000       # 0x18      0001 1000       24
-    Dry = 0b00010000        # 0x10      0001 0000       16
-    Auto = 0b00100000       # 0x20      0010 0000       32
+    Hot = 0b00001000            # 0x08      0000 1000        8
+    Cold = 0b00011000           # 0x18      0001 1000       24
+    Dry = 0b00010000            # 0x10      0001 0000       16
+    Auto = 0b00100000           # 0x20      0010 0000       32
+
+    __Hot2 = 0b00001000         # 0x00      0000 0000        0
+    __Cold2 = 0b00011000        # 0x06      0000 0110        6
+    __Dry2 = 0b00010000         # 0x02      0000 0010        2
+    __Auto2 = 0b00100000        # 0x00      0000 0000        0
+
+    @classmethod
+    def climate2(cls, climate_mode):
+        """
+        climate2: Converts to the second climate value (For ClimateAndHorizontalVanne)
+        """
+        if climate_mode == cls.Hot:
+            return cls.__Hot2
+        if climate_mode == cls.Cold:
+            return cls.__Cold2
+        if climate_mode == cls.Dry:
+            return cls.__Dry2
+        if climate_mode == cls.Auto:
+            return cls.__Auto2
+
+
+class ISeeMode:
+    """
+    ISeeMode
+    """
+    NotAvailable = 0b00000000   # 0x00      0000 0000        0
+    ISeeOff = 0b00000000        # 0x00      0000 0000        0
+    ISeeOn = 0b01000000         # 0x40      0100 0000       64
+
+class VanneHorizontalMode:
+    """
+    VanneHorizontalMode
+    """
+    NotAvailable = 0b00000000   # 0x00      0000 0000        0
+    LeftEnd = 0b01000000        # 0x10      0001 0000       16
+    Left = 0b01001000           # 0x20      0010 0000       32
+    Middle = 0b01010000         # 0x30      0011 0000       48
+    Right = 0b01011000          # 0x40      0100 0000       64
+    RightEnd = 0b01100000       # 0x50      0101 0000       80
+    Swing = 0b01101000          # 0x80      1000 0000      128
 
 class FanMode:
     """
     FanMode
     """
-    Speed1 = 0b00000001     # 0x01      0000 0001        1
-    Speed2 = 0b00000010     # 0x02      0000 0010        2
-    Speed3 = 0b00000011     # 0x03      0000 0011        3
-    Speed4 = 0b00000100     # 0x04      0000 0100        4
-    Auto = 0b10000000       # 0x80      1000 0000      128
-    Silent = 0b00000101     # 0x05      0000 0101        5
+    Speed1 = 0b00000001         # 0x01      0000 0001        1
+    Speed2 = 0b00000010         # 0x02      0000 0010        2
+    Speed3 = 0b00000011         # 0x03      0000 0011        3
+    Speed4 = 0b00000100         # 0x04      0000 0100        4
+    Auto = 0b10000000           # 0x80      1000 0000      128
+    Silent = 0b00000101         # 0x05      0000 0101        5
 
-class WideVanneMode:
+class VanneVerticalMode:
     """
-    WideVanneMode
+    VanneVerticalMode
     """
-    Auto = 0b01000000       # 0x40      0100 0000       64
-    WvH1 = 0b01001000       # 0x48      0100 1000       72
-    WvH2 = 0b01010000       # 0x50      0101 0000       80
-    WvH3 = 0b01011000       # 0x58      0101 1000       88
-    WvH4 = 0b01100000       # 0x60      0110 0000       96
-    WvH5 = 0b01101000       # 0x68      0110 1000      104
-    AutoMove = 0b01111000   # 0x78      0111 1000      120
+    Auto = 0b01000000           # 0x40      0100 0000       64
+    WvH1 = 0b01001000           # 0x48      0100 1000       72
+    WvH2 = 0b01010000           # 0x50      0101 0000       80
+    WvH3 = 0b01011000           # 0x58      0101 1000       88
+    WvH4 = 0b01100000           # 0x60      0110 0000       96
+    WvH5 = 0b01101000           # 0x68      0110 1000      104
+    Swing = 0b01111000          # 0x78      0111 1000      120
 
+class TimeControlMode:
+    """
+    TimeControlMode
+    """
+    NoTimeControl = 0b00000000  # 0x00      0000 0000        0
+    ControlStart = 0b00000000   # 0x05      0000 0101        5
+    ControlEnd = 0b00000000     # 0x03      0000 0011        3
+    ControlBoth = 0b00000000    # 0x07      0000 0111        7
+
+class AreaMode:
+    """
+    AreaMode
+    """
+    NotAvailable = 0b00000000   # 0x00      0000 0000        0
+    NotSet = 0b00000000         # 0x00      0000 0000        0
+    Left = 0b00000000           # 0x40      0100 0000       64
+    Right = 0b00000000          # 0xC0      1100 0000      192
+    Full = 0b00000000           # 0x80      1000 0000      128
+    
 class Delay:
     """
     Delay
@@ -63,11 +123,24 @@ class Index:
     """
     Index
     """
-    Power = 5               # Byte 6 - On / Off
-    Climate = 6             # Byte 7 - Mode
-    Temperature = 7         # Byte 8 - Temperature
-    FanVanne = 9            # Byte 10 - FAN / VANNE
-    CRC = 17                # Byte 18 - CRC
+    Header0 = 0
+    Header1 = 1
+    Header2 = 2
+    Header3 = 3
+    Header4 = 4
+    Power = 5
+    ClimateAndISee = 6
+    Temperature = 7
+    ClimateAndHorizontalVanne = 8
+    FanAndVerticalVanne = 9
+    Clock = 10
+    EndTime = 11
+    StartTime = 12
+    TimeControlAndArea = 13
+    Unused14 = 14
+    Unused15 = 15
+    Unused16 = 16
+    CRC = 17
 
 class Constants:
     """
@@ -84,7 +157,7 @@ class Mitsubishi:
     """
     Mitsubishi
     """
-    def __init__(self, gpio_pin, log_level = ir_sender.LogLevel.Minimal):
+    def __init__(self, gpio_pin, log_level=ir_sender.LogLevel.Minimal):
         self.log_level = log_level
         self.gpio_pin = gpio_pin
 
@@ -96,10 +169,24 @@ class Mitsubishi:
             ClimateMode.Auto,
             21,
             FanMode.Auto,
-            WideVanneMode.Auto,
+            VanneVerticalMode.Auto,
+            VanneHorizontalMode.Swing,
+            ISeeMode.NotAvailable,
+            AreaMode.NotAvailable,
+            None,
+            None,
             PowerMode.PowerOff)
 
-    def send_command(self, climate_mode, temperature, fan_mode, wide_vanne_mode):
+    def send_command(self,
+                     climate_mode=ClimateMode.Auto,
+                     temperature=21,
+                     fan_mode=FanMode.Auto,
+                     vanne_vertical_mode=VanneVerticalMode.Auto,
+                     vanne_horizontal_mode=VanneHorizontalMode.NotAvailable,
+                     isee_mode=ISeeMode.NotAvailable,
+                     area_mode=AreaMode.NotAvailable,
+                     start_time=None,
+                     end_time=None):
         """
         send_command
         """
@@ -107,10 +194,15 @@ class Mitsubishi:
             climate_mode,
             temperature,
             fan_mode,
-            wide_vanne_mode,
+            vanne_vertical_mode,
+            vanne_horizontal_mode,
+            isee_mode,
+            area_mode,
+            start_time,
+            end_time,
             PowerMode.PowerOn)
 
-    def __send_command(self, climate_mode, temperature, fan_mode, wide_vanne_mode, power_mode):
+    def __send_command(self, climate_mode, temperature, fan_mode, vanne_vertical_mode, vanne_horizontal_mode, isee_mode, area_mode, start_time, end_time, power_mode):
 
         sender = ir_sender.IrSender(self.gpio_pin, "NEC", dict(
             leading_pulse_duration=Delay.HdrMark,
@@ -128,11 +220,30 @@ class Mitsubishi:
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F]
 
         data[Index.Power] = power_mode
-        data[Index.Climate] = climate_mode
+        data[Index.ClimateAndISee] = climate_mode | isee_mode
         data[Index.Temperature] = max(Constants.MinTemp, min(Constants.MaxTemp, temperature)) - 16
-        data[Index.FanVanne] = fan_mode | wide_vanne_mode
+        data[Index.ClimateAndHorizontalVanne] = ClimateMode.climate2(climate_mode) | vanne_horizontal_mode
+        data[Index.FanAndVerticalVanne] = fan_mode | vanne_vertical_mode
+
+        now = datetime.today()
+        data[Index.Clock] = now.hour * (now.minute//10)
+
+        data[Index.EndTime] = 0 if end_time is None else (end_time.hour * (end_time.minute//10))
+        data[Index.StartTime] = 0 if start_time is None else start_time.hour * (start_time.minute//10)
+
+        time_control = TimeControlMode.NoTimeControl
+        if end_time is not None and start_time is not None:
+            time_control = TimeControlMode.ControlBoth
+        elif end_time is not None:
+            time_control = TimeControlMode.ControlEnd
+        elif start_time is not None:
+            time_control = TimeControlMode.ControlStart
+        else:
+            time_control = TimeControlMode.NoTimeControl
+        data[Index.TimeControlAndArea] = time_control | area_mode 
 
         # CRC is a simple bits addition
-        data[Index.CRC] = sum(data[:-1]) % (Constants.MaxMask + 1) # sum every bytes but the last one
+        # sum every bytes but the last one
+        data[Index.CRC] = sum(data[:-1]) % (Constants.MaxMask + 1)
 
         sender.send_data(data, Constants.MaxMask, True, Constants.NbPackets)
--- python/hvac_ircontrol/version.py
+++ python/hvac_ircontrol/version.py
@@ -1,13 +1,13 @@
 # Version information.  
 _version_major = 1
-_version_minor = 0
+_version_minor = 1
 _version_micro = 0
 
 
 # An empty _version_extra corresponds to a full release.
 # '.dev' as a _version_extra string means this is a development version
-_version_extra = ''
-# _version_extra = 'dev'
+#_version_extra = ''
+_version_extra = 'dev'
 
 
 # Format expected by setup.py: string of form "X.Y.Z"
