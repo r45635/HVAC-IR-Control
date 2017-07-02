82b8b40f4db53e4aa76bf980ef6925dcf3a63574
Éric Massé <Ericmas001@gmail.com>
2017-07-02 01:21:41 -0400

    adjusting demo

--- python/demo_mitsu.py
+++ python/demo_mitsu.py
@@ -4,8 +4,9 @@
 Demo Mitsubishi HVAC
 """
 import time
+import datetime
 from hvac_ircontrol.ir_sender import LogLevel
-from hvac_ircontrol.mitsubishi import ClimateMode, FanMode, WideVanneMode, Mitsubishi
+from hvac_ircontrol.mitsubishi import Mitsubishi, ClimateMode, FanMode, VanneVerticalMode, VanneHorizontalMode, ISeeMode, AreaMode
 
 if __name__ == "__main__":
     while True:
@@ -16,7 +17,15 @@ if __name__ == "__main__":
         print("Wait 2 secs ...")
         time.sleep(2)
         print("It's gonna get cold here !")
-        HVAC.send_command(ClimateMode.Cold, 18, FanMode.Speed4, WideVanneMode.Auto)
+        HVAC.send_command(
+            climate_mode=ClimateMode.Auto,
+            temperature=23,
+            fan_mode=FanMode.Auto,
+            vanne_vertical_mode=VanneVerticalMode.Auto,
+            vanne_horizontal_mode=VanneHorizontalMode.NotAvailable,
+            isee_mode=ISeeMode.ISeeOn,
+            area_mode=AreaMode.Full
+            )
         print("=======================================================")
         print("Go dormant for 30 secs ...")
         time.sleep(25)
